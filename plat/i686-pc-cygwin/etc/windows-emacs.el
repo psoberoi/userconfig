8880cb89647da208b0b02b724cf64e75204bbbda
Paramjit Oberoi <paramjit@amazon.com>
2011-05-12 13:48:21 -0700

    add C: to aspell path

--- plat/i686-pc-cygwin/etc/windows-emacs.el
+++ plat/i686-pc-cygwin/etc/windows-emacs.el
@@ -1,2 +1,2 @@
 (load "/cygwin/home/paramjit/userconfig/init/emacs.el")
-(setq ispell-program-name "/Program Files (x86)/Aspell/bin/aspell.exe")
+(setq ispell-program-name "C:/Program Files (x86)/Aspell/bin/aspell.exe")
