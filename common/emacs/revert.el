9552859ab8bbaf57dda22bb68ffcce4e01e4195f
Paramjit Oberoi <paramjit@amazon.com>
2011-09-19 10:34:14 -0700

    added an implementation of revert-all-buffers

---
+++ common/emacs/revert.el
@@ -0,0 +1,9 @@
+(defun revert-all-buffers ()
+  "Refreshes all open buffers from their respective files."
+  (interactive)
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (when (and (buffer-file-name) (not (buffer-modified-p)))
+        (revert-buffer t t t) )))
+  (message "Refreshed open files.") )
+
