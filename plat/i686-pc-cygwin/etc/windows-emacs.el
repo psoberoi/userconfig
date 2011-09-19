aa3c86453952f81082c69113ccaa257d9e022f30
Paramjit Oberoi <paramjit@amazon.com>
2011-09-19 11:24:38 -0700

    load tramp and set default method to plink on windows

--- plat/i686-pc-cygwin/etc/windows-emacs.el
+++ plat/i686-pc-cygwin/etc/windows-emacs.el
@@ -1,2 +1,5 @@
+; Place in ~/Win/AppData/Roaming/.emacs.el
 (load "/cygwin/home/paramjit/userconfig/init/emacs.el")
 (setq ispell-program-name "C:/Program Files (x86)/Aspell/bin/aspell.exe")
+(require 'tramp)
+(setq tramp-default-method "plink")
